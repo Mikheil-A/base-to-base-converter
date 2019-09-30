6c8d1d294840791abb8135377ad87a30dd7ecabd
Mikheil Aleksidze <mikheilaleksidze@gmail.com>
2019-09-30 11:15:26 +0400

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'binary-to-decimal-converter';
+
+  convert(e) {
+    const inputValue = e['srcElement'].value;
+    console.log((inputValue >>> 0).toString(2));
+
+  }
+
 }
